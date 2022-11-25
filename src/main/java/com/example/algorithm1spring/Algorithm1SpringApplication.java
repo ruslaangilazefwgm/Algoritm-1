49b376e2406601dc5adab8acebd67cd6e0f3ecde
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-11-25 17:08:23 +0300

    hw

---
+++ src/main/java/com/example/algorithm1spring/Algorithm1SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithm1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithm1SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithm1SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithm1spring/Algorithm1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithm1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algorithm1SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
